33a0d9c4f1267dcc1d754df9ab4132662e743d4f
scneeman <30477959+scneeman@users.noreply.github.com>
2024-03-07 13:06:24 -0600

    Add files via upload

---
+++ buttons.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+# define any functions
+
+ 
+# setup main window
+
+
+# create frame 1
+
+
+# create frame 2
+
+
+# 'raise' the frame you want visible first
---
+++ your_turn.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+# section 1: define any functions
+
+
+# section 2: setup main window
+
+
+# section 3: create frames and widgets
+
+
+# the 'contact us' screen
+
+
+# the 'about us' screen
+
+
+# section 4: the main loop
+
